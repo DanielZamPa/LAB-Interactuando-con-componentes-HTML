3ac9bb4d07c61642976b5644548ef7516e658fdc
Daniel Zamora Paez <[email>
2025-07-22 21:29:39 -0500

    Funcionalidades de la encuesta

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ let voto3 = 0;
 document.getElementById("voto1").addEventListener("click", function(){
     voto1+=1;
     document.getElementById('opcion1').textContent=voto1;
+    alert("¡GRACIS POR TU VOTO!");
+    if (voto1%5==0){
+        console.log("Cantidad de votos seleccion 1: ",voto1);
+    }
 })
 document.getElementById("voto2").addEventListener("click", function(){
     voto2+=1;
     document.getElementById('opcion2').textContent=voto2;
+    alert("¡GRACIS POR TU VOTO!");
+    if (voto2%5==0){
+        console.log("Cantidad de votos seleccion 2: ",voto2);
+    }
 })
 document.getElementById("voto3").addEventListener("click", function(){
     voto3+=1;
     document.getElementById('opcion3').textContent=voto3;
+    alert("¡GRACIS POR TU VOTO!");
+    if (voto3%5==0){
+        console.log("Cantidad de votos seleccion 3: ",voto3);
+    }
 })
