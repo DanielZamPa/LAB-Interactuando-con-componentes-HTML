f5adc188537c0bbd2e9da8caa25996a5ea0ab78c
Daniel Zamora Paez <[email>
2025-07-22 21:01:35 -0500

    Cambios estructura HTML

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const prompt = require("prompt-sync")();
-let voto1,voto2,voto3 = 0;
-document.getElementsById("voto1").addEventListener("click", function(){
+let voto1 = 0;
+let voto2 = 0;
+let voto3 = 0;
+document.getElementById("voto1").addEventListener("click", function(){
     voto1+=1;
     document.getElementById('opcion1').textContent=voto1;
 })
-document.getElementsById("voto2").addEventListener("click", function(){
+document.getElementById("voto2").addEventListener("click", function(){
     voto2+=1;
     document.getElementById('opcion2').textContent=voto2;
 })
-document.getElementsById("voto3").addEventListener("click", function(){
-    voto1+=1;
+document.getElementById("voto3").addEventListener("click", function(){
+    voto3+=1;
     document.getElementById('opcion3').textContent=voto3;
 })
